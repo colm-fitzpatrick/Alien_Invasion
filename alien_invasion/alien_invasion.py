68dd3d548a776ef43458f5adba1c7213be218b14
Colm Fitzpatrick <cfitzpatrick369@gmail.com>
2019-11-13 08:50:48 +0000

    Alien fleet update
    Changed starting position of alien fleet

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -190,7 +190,7 @@ class AlienInvasion:
         alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
-        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        alien.rect.y = 2 * alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _check_fleet_edges(self):
